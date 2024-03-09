1e78c1d43f1df93e684137029dd154ab963543be
ydgin5 <ydginnuch5@gmail.com>
2024-03-09 15:26:38 +0300

    new commit

---
+++ style.js
@@ -0,0 +1,20 @@
+"use strict";
+
+window.addEventListener("load", windowLoaded);
+function windowLoaded() {
+  if (document.querySelector(".product__slider")) {
+    new Swiper(".product__slider",{
+    
+      loop: true,
+    speed:1000,
+    parallax:true,
+    mousewhell:true,
+    keyboard:true,
+    on:{
+        init:function(){
+            document.documentElement.classList.add("loaded");
+        }
+    }
+  });
+}
+}
